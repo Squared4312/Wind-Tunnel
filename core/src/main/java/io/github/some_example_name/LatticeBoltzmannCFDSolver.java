1ba134880e4ee7cc9590695c4aa1f97c9742c746
Nathan <becker.nathan4312@gmail.com>
2026-05-20 13:27:37 +0100

    hashmap start?

--- core/src/main/java/io/github/some_example_name/LatticeBoltzmannCFDSolver.java
+++ core/src/main/java/io/github/some_example_name/LatticeBoltzmannCFDSolver.java
@@ -1,28 +1,22 @@
 package io.github.some_example_name;
 
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+
+import java.util.HashMap;
 
 public class LatticeBoltzmannCFDSolver {
     private MenuUtil util;
     private Settings settings;
 
     // setup mesh
-    private Vector2 numberOfCells;
-    private Vector2 cellDimensions;
+    private Vector3 numberOfCells;
+    private Vector3 cellDimensions;
 
-    // these arrays of densities are named by their relative offset to the cell (in 3D), where _1 = -1
-    private double[][] density000;
-    private double[][] density010;
-    private double[][] density0_10;
-    private double[][] density100;
-    private double[][] density_100;
-    private double[][] density_110;
-    private double[][] density110;
-    private double[][] density_1_10;
-    private double[][] density1_10;
-
-    private double[][] density101;
+    // array of densities named by their relative offset to the cell (in 3D)
+    private HashMap<String, Double>[][][] densities;
+    private Integer[] directions = {-1, 0, 1};
+    private Integer[][] invalidDirections = {{-1, 1, -1}, {-1, -1, -1}, {1, 1, -1}, {1, -1, -1}, {-1, 1, 1}, {-1, -1, 1}, {1, 1, 1}, {1, -1, 1}};
 
     public LatticeBoltzmannCFDSolver() {
         this.util = new MenuUtil();
@@ -31,19 +25,27 @@ public class LatticeBoltzmannCFDSolver {
     }
 
     public void initialiseCells() {
-        this.numberOfCells = new Vector2(settings.getResolution().x, settings.getResolution().y);
-        this.cellDimensions = new Vector2(util.getScreenDimensions().x/numberOfCells.x, util.getScreenDimensions().y/numberOfCells.y);
-        this.density000 = new double[(int) numberOfCells.x][(int) numberOfCells.y];
-        this.density010 = new double[(int) numberOfCells.x][(int) numberOfCells.y];
-        this.density0_10 = new double[(int) numberOfCells.x][(int) numberOfCells.y];
-        this.density100 = new double[(int) numberOfCells.x][(int) numberOfCells.y];
-        this.density_100 = new double[(int) numberOfCells.x][(int) numberOfCells.y];
-        this.density_110 = new double[(int) numberOfCells.x][(int) numberOfCells.y];
-        this.density110 = new double[(int) numberOfCells.x][(int) numberOfCells.y];
-        this.density_1_10 = new double[(int) numberOfCells.x][(int) numberOfCells.y];
-        this.density1_10 = new double[(int) numberOfCells.x][(int) numberOfCells.y];
+        this.numberOfCells = new Vector3(settings.getResolution().x, settings.getResolution().y, settings.getResolution().z);
+        this.cellDimensions = new Vector3(util.getScreenDimensions().x/numberOfCells.x, util.getScreenDimensions().y/numberOfCells.y, util.getScreenDimensions().y/numberOfCells.z);
+        this.densities = new HashMap<String, Double>[(int) numberOfCells.x][(int) numberOfCells.y][(int) numberOfCells.z];
 
-        this.density101 = new double[(int) numberOfCells.x][(int) numberOfCells.y];
+        boolean skip;
+        for (int x : directions) {
+            for (int y : directions) {
+                for (int z : directions) {
+                    skip = false;
+                    for (Integer[] invalid : invalidDirections) {
+                        if (x == invalid[0] && y == invalid[1] && z == invalid[2]) {
+                            skip = true;
+                            break;
+                        }
+                    }
+                    if (skip) {break;}
+
+                    densities.put(String.valueOf(x) + String.valueOf(y) + String.valueOf(z), 0.0);
+                }
+            }
+        }
     }
 }
 
